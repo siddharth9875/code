6031d4c52dba6de0baf352bce0792a1379db6b7f
siddharth9875 <pushtiwebs@gmail.com>
2023-04-09 11:09:17 -0400

    Changes to be committed:

--- java/concept/getting_user_input.java
+++ java/concept/getting_user_input.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-public class getting_user_input{
-    public static void main(String[] args){
-           Scanner sc = new Scanner(System.in);
-           System.out.println("please enter value a");
-           int a = sc.nextInt();
-           System.out.println("please enter value b");
-           int b = sc.nextInt();
-           int c = a+b;
-           System.out.println(c);
+public class getting_user_input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("please enter value a");
+        int a = sc.nextInt();
+        System.out.println("please enter value b");
+        int b = sc.nextInt();
+        int c = a + b;
+        System.out.println(c);
     }
 }
--- java/concept/operator_expression.java
+++ java/concept/operator_expression.java
@@ -16,13 +16,13 @@ public class operator_expression {
 
         // 4. Logical Operators
         // System.out.println(64>5 && 64>98);
-        System.out.println(64>5 || 64>98);
+        System.out.println(64 > 5 || 64 > 98);
 
         // 5. Bitwise Operators
-        System.out.println(2&3);
-        //        10
-        //        11
-        //        ----
-        //        10
+        System.out.println(2 & 3);
+        // 10
+        // 11
+        // ----
+        // 10
     }
 }
